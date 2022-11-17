a7e339a59892629919a1954edd510870141dc36c
Sávio Haistém <savio.dantas.5858@gmail.com>
2022-11-17 17:29:46 -0300

    adicionando funções basicas

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,2 +1,23 @@
-const TIME = new Date 
+class timer {
+    constructor(Time) {
+        this.timer = { Time };
+        this.time = document.querySelector('.time')
+    }
+    visor() {
+        this.time.innerHTML = `${this.timer.Time}`
+    }
+    plustime() {
+        this.timer.Time >= 0 ? this.timer.Time += 1 : undefined
+        this.visor()
+    }
+    minustime() {
+        this.timer.Time > 0 ? this.timer.Time -= 1 : undefined
+        this.visor()
+    }
+    play() {
+        this.timer.Time = ()=>{}
+        this.visor()
+    }
+}
 
+const TIMER = new timer(0)
