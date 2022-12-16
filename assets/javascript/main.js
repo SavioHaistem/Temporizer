ba1697495d4ef4dab95f330a7f08d40cac981dfe
Sávio Haistém <savio.dantas.5858@gmail.com>
2022-12-16 09:42:24 -0300

    projeto finalizado

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,23 +1,64 @@
-class timer {
-    constructor(Time) {
-        this.timer = { Time };
-        this.time = document.querySelector('.time')
-    }
-    visor() {
-        this.time.innerHTML = `${this.timer.Time}`
-    }
-    plustime() {
-        this.timer.Time >= 0 ? this.timer.Time += 1 : undefined
-        this.visor()
-    }
-    minustime() {
-        this.timer.Time > 0 ? this.timer.Time -= 1 : undefined
-        this.visor()
-    }
-    play() {
-        this.timer.Time = ()=>{}
-        this.visor()
+var clock = {
+
+    seconds: 0,
+    minutes: 0,
+    hours: 0,
+
+
+    atualizar() {
+
+        const spaceHours = document.querySelector('.hours');
+        spaceHours.innerHTML = `${this.hours}:`
+
+        const spaceMinutes = document.querySelector('.minutes')
+        spaceMinutes.innerHTML = `${this.minutes}`
+
+        const spaceSeconds = document.querySelector('.seconds')
+        spaceSeconds.innerHTML = `:${this.seconds}`
+    },
+
+    adicionar() {
+
+        if ( this.seconds <  59 ) 
+        {
+            this.seconds += 1;
+        } 
+        else if ( this.minutes < 59 ) 
+        {
+            this.minutes += 1;
+            this.seconds = 0;
+        } else if ( this.hours < 24 ) 
+        {
+            this.hours += 1;
+            this.minutes = 0;
+            this.seconds = 0;
+        };
+        this.atualizar()
+    },
+
+    diminuir() {
+
+        if ( this.seconds > 0 ) 
+        {
+            this.seconds -= 1
+        } 
+        else if ( this.minutes > 0 ) 
+        {
+            this.minutes -= 1
+            this.seconds += 59
+        }
+        else if ( this.hours > 0 ) 
+        {
+            this.hours -= 1
+            this.minutes += 59
+        };
+        clock.atualizar()
+    },
+
+    play() 
+    {   
+        clock.diminuir()
+
+        clock.seconds > 0 ? setTimeout( clock.play, 1000 ) : clearInterval( clock.play )
     }
 }
-
-const TIMER = new timer(0)
